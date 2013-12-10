31ce1fd31fdaf799649d71afb38b6116d6e85215
Bronson Philippa <bronson.philippa@gmail.com>
2013-12-10 15:34:46 +1000

    Fixed start-workers-with-qsub.sh script

--- +server/start-workers-with-qsub.sh
+++ +server/start-workers-with-qsub.sh
@@ -12,9 +12,11 @@ fi
 [ "$#" -eq 2 ] || die "Usage: $0 server-hostname number-of-workers"
 
 hash="`date | md5sum | head -c10`"
+WORKER_DIR="$HOME/scratch/cache/matlab-job-manager/workers"
+mkdir -p "$WORKER_DIR"
 for i in $(seq 1 $2); do
-  stdout="$HOME/scratch/cache/organic-device-simulation/workers/${hash}_${i}.stdout"
-  stderr="$HOME/scratch/cache/organic-device-simulation/workers/${hash}_${i}.stderr"
-  qsub -d "`pwd`" -e "$stderr" -o "$stdout" -N "Worker_${hash}_${i}" -v "server_hostname=$1" "./worker-job.sh"
-  sleep 1
+  stdout="$WORKER_DIR/${hash}_${i}.stdout"
+  stderr="$WORKER_DIR/${hash}_${i}.stderr"
+  qsub -d "`pwd`" -e "$stderr" -o "$stdout" -N "Worker_${hash}_${i}" -v "server_hostname=$1" "./+jobmgr/+server/worker-job.sh"
+  sleep 0.1 # so as not to hammer the cluster's job scheduler
 done
