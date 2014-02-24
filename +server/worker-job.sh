448c112c9ac538fea8c614bcb1a618cd0ce40519
Bronson Philippa <bronson.philippa@gmail.com>
2014-02-25 08:53:48 +1000

    Increased PBS memory limit to 2gb

--- +server/worker-job.sh
+++ +server/worker-job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l pmem=1gb
+#PBS -l pmem=2gb
 #PBS -l nodes=1
 #PBS -l walltime=48:00:00
 
@@ -9,4 +9,7 @@ if [ -e "/etc/profile.d/modules.sh" ]; then
 fi
 
 echo "Starting Matlab..."
+# The argument -singleCompThread is used to be friendlier on shared
+# HPC systems. Otherwise Matlab seems to optimistically start many
+# threads even though most operations are single threaded.
 matlab -singleCompThread -r "jobmgr.server.start_worker('$server_hostname');"
