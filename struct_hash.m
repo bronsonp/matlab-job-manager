4bf3a8d6dd9a0c38b6d1b9cba0f1bab811beaaf1
Bronson Philippa <bronson.philippa@gmail.com>
2017-03-03 14:01:26 +1000

    Updated to SHA256 hash and fixed function handle path dependency
    Previously, function handle hashes would depend upon the file path, thus
    making hashes vary between computers. Function handles are now converted
    to simple strings before being hashes, improving robustness when moving
    configs between machines.

--- struct_hash.m
+++ struct_hash.m
@@ -1,25 +1,44 @@
-function h = struct_hash(struct)
+function h = struct_hash(s)
 % STRUCT_HASH Generate a hash of the supplied structure.
 %
-% H = STRUCT_HASH(STRUCT) returns a string hash of the structure STRUCT.
+% H = STRUCT_HASH(S) returns a string hash of the structure S.
 % This hash is intended to provide a unique fingerprint for any
-% structure. Uses the SHA1 algorithm.
+% structure. Uses the SHA-256 algorithm.
 %
-% Implementation: The structure is serialized and the result is
-% then hashed. The serialization takes care to store the fields in
-% alphabetical order, so that differences in field order do not
-% affect the structure hash.
+% Implementation: The structure is serialized using the internal Matlab
+% function getByteStreamFromArray and then hashed.
+% 
+% Function handles are treated specially. Matlab's native serialisation
+% routine includes the Matlabroot path in the function handle, so it's not
+% stable across multiple machines. Therefore function handles are converted
+% to a string before hashing.
 %
 
     persistent md;
     if isempty(md)
-        % Load the SHA1 hash algorithm from Java
+        % Load the hash algorithm from Java
         md = java.security.MessageDigest.getInstance('SHA-256');
     end
     md.reset();
 
+    % convert any function handles to strings
+    function s = sanitise_struct(s)
+        fields = fieldnames(s);
+        for i = 1:numel(fields)
+            field = fields{i};
+            if isa(s.(field), 'function_handle')
+                s.(field) = ['function_handle: ' char(s.(field))];
+            elseif isstruct(s.(field)) || isobject(s.(field))
+                for j = 1:numel(s.(field))
+                    s.(field)(j) = sanitise_struct(s.(field)(j));
+                end
+            end
+        end
+    end
+    s = sanitise_struct(s);
+    
     % serialise
-    s = getByteStreamFromArray(struct);
+    s = getByteStreamFromArray(s);
 
     % hash
     md.update(s);
